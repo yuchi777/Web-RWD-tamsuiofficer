bb7fa6554588f79b8c57b46e5a87363c24267df1
810lu <xm3m4xup6@gmail.com>
2021-12-08 15:54:20 +0800

    Identity authentication

--- backend/header.js
+++ backend/header.js
@@ -1,4 +1,4 @@
-header = `
+$("#header").html(`
 <div class="header">
     <div class="title">
         <a href="./A100.html" class="bd_index">
@@ -9,9 +9,9 @@ header = `
     <div class="nav_bar">
         <nav>
             <ul>
-                <li><a href="./B100.html">新聞傳播及活動消息管理</a></li>
-                <li><a href="./C100.html">詮釋資料管理</a></li>
-                <li><a style="cursor: pointer;">系統管理</a>
+                <li class="user-status common"><a href="./B100.html">新聞傳播及活動消息管理</a></li>
+                <li class="user-status common"><a href="./C100.html">詮釋資料管理</a></li>
+                <li class="user-status admin"><a style="cursor: pointer;" >系統管理</a>
                 <ul>
                     <li><a href="./D100.html">帳號管理</a></li>
                     <li><a href="./D200.html">權限管理</a></li>
@@ -20,10 +20,50 @@ header = `
                 </li>
             </ul>
         </nav>
-        <a class="log_out" href="L100.html">登出</a>
+        <a class="user-status log_out" href="L100.html" type="submit">登出</a>
         <p>|</p>
-        <p class="identity">系統管理員</p>
-    </div>
-</div>
-`
-document.write(header);
+        <p class="admin">系統管理員</p>
+        <p class="normal">維護專員</p>
+        </div>
+        </div>
+        `);
+
+var id = localStorage.getItem("id");
+console.log(id);
+
+// 沒登入直接輸入網址
+if (!id){
+    document.location.href="L100.html";
+}
+
+// 登入驗證會員
+if (id) {
+  var loginUSer = window.DB.find(function (dbData) {
+    return dbData.id === id;
+  });
+  if (!loginUSer || window.location.pathname === "/") {
+    // do nothing
+  } else if (loginUSer.isAdmin) {
+    $(".admin").show();
+    $(".common").show();
+    $(".log_out").show();
+    $(".access_rights").hide();
+    $(".normal").hide();
+    
+  } else {
+    $(".admin").hide();
+    $(".common").show();
+    $(".log_out").show();
+    $(".D-series").hide();
+    $(".access_rights").show();
+  }
+}
+
+if ($(".D-access_rights").show() && window.location.pathname.includes("D")) {
+    setTimeout(function(){ history.go(-1)} ,3000)
+};
+
+// 登出清除local storage資料
+$(".log_out").click(function(){
+    localStorage.clear();
+});
