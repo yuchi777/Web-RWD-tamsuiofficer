cb2569f5df34aea494753299e03e375fe9f9ff55
Ravey-chen <zxcv7879105@gmail.com>
2021-12-09 15:51:55 +0800

    up A300

--- backend/js/A300.js
+++ backend/js/A300.js
@@ -1,42 +1,15 @@
-async function getUsers() {
-    let url = 'A300.json';
-    try {
-        let res = await fetch(url);
-        return await res.json();
-    } catch (error) {
-        console.log(error);
-    }
-}
-async function renderUsers() {
-    let users = await getUsers();
-    let html = '';
-    users.forEach(A300 => {
-        let htmlSegment = `
-           <div class="A300">
-             <table>
-               <thead>
-                 <tr>
-                   <td><a href="./A400.html"><img src="${A300.a300_img}"></td>
-                   
-                   <td>${A300.a300_1} </td>
-                   <td>${A300.a300_2} </td>
-                   <td>${A300.a300_3} </td>
-                   <td>${A300.a300_4} </td>
-                   <td>${A300.a300_5} </td>
-                   <td>${A300.a300_6} </td>
-                   <td></td>
-                   <td>${A300.a300_7} </td>
-                   <td>${A300.a300_8} </td>
-                 </tr> 
-               </thead>
-             </table>
-           </div>`;
-
-        html += htmlSegment;
-    });
-
-    let A300table = document.querySelector('.A300table');
-    A300table.innerHTML = html;
-}
-
-renderUsers();
+$(document).ready(function () {
+  $('#example').DataTable({
+      "ajax": "A300.json",
+      "columns": [
+          { "data": "name" },
+          { "data": "position" },
+          { "data": "office" },
+          { "data": "extn" },
+          { "data": "start_date" },
+          { "data": "salary" },
+          { "data": "date" },
+          { "data": "time" },
+      ]
+  });
+});
